7b6e7f393a0d7497eb3b4f3ea397e602625360d1
bobbykim89 <bobby.sihun.kim@gmail.com>
2022-07-07 01:56:47 -0700

    add inline event handlers and conditionals

--- src/App.svelte
+++ src/App.svelte
@@ -1,60 +1,39 @@
 <script>
   import logo from '@/assets/svelte.png'
   import Counter from '@/lib/Counter.svelte'
-  let name = 'Manguito'
-  let beltColor = 'black'
-
-  let firstName = 'Pollo'
-  let lastName = 'Chicken'
-
   let people = [
     { name: 'yoshi', beltColour: 'black', age: 25, id: 1 },
     { name: 'mario', beltColour: 'orange', age: 45, id: 2 },
     { name: 'luigi', beltColour: 'brown', age: 35, id: 3 }
   ];
 
-  const svelteUrl = "https://svelte.dev"
-  const svelteKitUrl = "https://github.com/sveltejs/kit#readme"
-
-  $: fullName = `${firstName} ${lastName}`
-  $: {
-    console.log(beltColor)
-    console.log(fullName)
+  const handleClick = (id) => {
+    // delete person from people
+    people = people.filter((person) => person.id !== id)
   }
 
-  const handleClick = () => {
-    beltColor = 'orange'
-  }
-
-  const handleInput = (e) => {
-    beltColor = e.target.value
-  }
+  let num = 5
 </script>
 
+{#if num > 20}
+  <p>Greater than 20</p>
+{:else if num > 5}
+  <p>Greater than 5</p>
+{:else}
+  <p>Not greater than 5</p>
+{/if}
+
 <main>
-  <img src={logo} alt="Svelte Logo" />
-  <h1>Hello {name}!</h1>
-  <button on:click={handleClick} style="color: {beltColor}">{beltColor} belt</button>
-  <!-- <input type="text" on:input={handleInput} value={beltColor}> -->
-  <p>{fullName}</p>
-  <input type="text" bind:value={beltColor}>
-  <input type="text" bind:value={firstName}>
-  <input type="text" bind:value={lastName}>
-  <Counter />
-
-  <p>
-    Visit <a href={svelteUrl} >svelte.dev</a> to learn how to build Svelte
-    apps.
-  </p>
-
-  <p>
-    Check out <a href={svelteKitUrl}>SvelteKit</a> for
-    the officially supported framework, also powered by Vite!
-  </p>
   {#each people as person (person.id)}
     <div>
       <h4>{person.name}</h4>
+      {#if person.beltColour === 'black'}
+        <p><strong>MASTER NINJA</strong></p>
+      {/if}
       <p>{person.age} years old, {person.beltColour} belt.</p>
+      <button on:click={() => 
+        handleClick(person.id)
+      }>Delete</button>
     </div>
   {:else}
     <p>There are no people to show...</p>
